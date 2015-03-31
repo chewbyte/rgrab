228017d6914ef44a65131650d6a9316061ebee43
Chris C <chriscola@outlook.com>
2015-03-31 21:29:46 +0100

    Added javafx skeleton code and a few tests

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,16 @@
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 
-public class Main extends Application {
+public class Main extends Application implements EventHandler<ActionEvent> {
 
     Button button;
+    Stage primary;
 
     public static void main(String[] args) {
         launch(args);
@@ -15,8 +18,10 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        primaryStage.setTitle("rgrab");
+        primary = primaryStage;
+        primary.setTitle("rgrab");
         button = new Button("rgrab");
+        button.setOnAction(this);
 
         StackPane layout = new StackPane();
         layout.getChildren().add(button);
@@ -24,5 +29,12 @@ public class Main extends Application {
         Scene scene = new Scene(layout,640,480);
         primaryStage.setScene(scene);
         primaryStage.show();
+    }
+
+    @Override
+    public void handle(ActionEvent e) {
+        if(e.getSource()==button){
+            primary.setTitle("topkek");
+        }
     }
 }
