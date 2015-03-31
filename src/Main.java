2cacbcf3cd7aa51011e620949880039370db1ea7
Chris C <chriscola@outlook.com>
2015-03-31 21:06:38 +0100

    Set up javafx stage/scene

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,13 @@
 
-import javafx.*;
 import javafx.application.Application;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 
 public class Main extends Application {
+
+    Button button;
 
     public static void main(String[] args) {
         launch(args);
@@ -11,6 +15,14 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("rgrab");
+        button = new Button("rgrab");
+
+        StackPane layout = new StackPane();
+        layout.getChildren().add(button);
+
+        Scene scene = new Scene(layout,640,480);
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
 }
-
