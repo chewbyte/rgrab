a4bc80cd8e8e7b3b26228495ef77e08795cbc40c
Chris C <chriscola@outlook.com>
2015-03-31 21:57:10 +0100

    Set onCloseRequest

--- src/Main.java
+++ src/Main.java
@@ -7,10 +7,11 @@ import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 
-public class Main extends Application implements EventHandler<ActionEvent> {
+public class Main extends Application{
 
     Button button;
     Stage primary;
+    Scene scene1, scene2;
 
     public static void main(String[] args) {
         launch(args);
@@ -20,21 +21,22 @@ public class Main extends Application implements EventHandler<ActionEvent> {
     public void start(Stage primaryStage) throws Exception {
         primary = primaryStage;
         primary.setTitle("rgrab");
+        primary.setOnCloseRequest(e -> closeProgram());
         button = new Button("rgrab");
-        button.setOnAction(this);
+        button.setOnAction(e -> {
+            primary.setTitle(primary.getTitle()+" X");
+        });
 
         StackPane layout = new StackPane();
         layout.getChildren().add(button);
 
-        Scene scene = new Scene(layout,640,480);
-        primaryStage.setScene(scene);
-        primaryStage.show();
+        scene1 = new Scene(layout,640,480);
+        primary.setScene(scene1);
+        primary.show();
     }
 
-    @Override
-    public void handle(ActionEvent e) {
-        if(e.getSource()==button){
-            primary.setTitle("topkek");
-        }
+    public void closeProgram(){
+        System.out.println("Shutting down.");
+        primary.close();
     }
 }
