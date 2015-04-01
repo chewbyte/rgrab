9b779862d4505a69a91e5a281a4869e774d9d6d3
Chris C <chriscola@outlook.com>
2015-04-01 13:55:02 +0100

    Added MenuBar

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,17 @@
 
 import javafx.application.Application;
-import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
+import javafx.scene.Group;
+import javafx.scene.control.*;
 
 public class Main extends Application{
 
     Button button;
-    Stage primary;
-    Scene scene1, scene2;
+
+    Stage stage;
+    Scene scene;
 
     public static void main(String[] args) {
         launch(args);
@@ -17,25 +19,49 @@ public class Main extends Application{
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        primary = primaryStage;
-        primary.setTitle("rgrab");
-        primary.setResizable(false);
-        primary.setOnCloseRequest(e -> closeProgram());
+        stage = primaryStage;
+        stage.setTitle("rgrab");
+        stage.setResizable(false);
+        stage.setOnCloseRequest(e -> closeProgram());
+
+        Group rootGroup = new Group();
+        scene = new Scene(rootGroup,640,480,Color.ALICEBLUE);
+
         button = new Button("Add an X to the title");
         button.setOnAction(e -> {
-            primary.setTitle(primary.getTitle()+" X");
+            stage.setTitle(stage.getTitle()+" X");
         });
 
-        StackPane layout = new StackPane();
-        layout.getChildren().add(button);
+        //Set up menu bar
+        MenuBar menuBar = new MenuBar();
 
-        scene1 = new Scene(layout,640,480);
-        primary.setScene(scene1);
-        primary.show();
+        // Menu - File
+        Menu menuFile = new Menu("File");
+        MenuItem menuOpen = new MenuItem("Open");
+        MenuItem menuExit = new MenuItem("Exit");
+        menuFile.getItems().addAll(menuOpen,menuExit);
+
+        // Menu - Help
+        Menu menuHelp = new Menu("Help");
+        MenuItem menuViewHelp = new MenuItem("View Help");
+        MenuItem menuAbout = new MenuItem("About");
+        menuHelp.getItems().addAll(menuViewHelp,menuAbout);
+
+        // Add Menus to MenuBar
+        menuBar.getMenus().addAll(menuFile, menuHelp);
+
+        // Bind width of Menu to Stage width
+        menuBar.prefWidthProperty().bind(stage.widthProperty());
+
+        // Add MenuBar to Group
+        rootGroup.getChildren().addAll(menuBar);
+        
+        stage.setScene(scene);
+        stage.show();
     }
 
     public void closeProgram(){
         System.out.println("Shutting down.");
-        primary.close();
+        stage.close();
     }
 }
